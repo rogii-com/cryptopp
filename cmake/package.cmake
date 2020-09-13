5bda4421dee794a9edc734ecf2bb0e4017964a72
k.khoroshevskiy <k.khoroshevskiy@rogii.com>
2020-09-13 09:39:23 +0400

    Remove unnecessary vars

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -2,16 +2,6 @@ if(TARGET Cryptopp)
     return()
 endif()
 
-set(
-    CRYPTOPP_INCLUDE_PATH
-    "${CMAKE_CURRENT_LIST_DIR}/include"
-)
-
-set(
-    CRYPTOPP_STATIC_LIBRARY_PATH
-    "${CMAKE_CURRENT_LIST_DIR}/lib"
-)
-
 add_library(
     Cryptopp
     STATIC
@@ -23,21 +13,21 @@ if(MSVC)
         Cryptopp
         PROPERTIES
         IMPORTED_LOCATION
-            "${CRYPTOPP_STATIC_LIBRARY_PATH}/cryptlib.lib"
+            "${CMAKE_CURRENT_LIST_DIR}/lib/cryptlib.lib"
         IMPORTED_LOCATION_DEBUG
-            "${CRYPTOPP_STATIC_LIBRARY_PATH}/cryptlibd.lib"
+            "${CMAKE_CURRENT_LIST_DIR}/lib/cryptlibd.lib"
         INTERFACE_INCLUDE_DIRECTORIES
-            "${CRYPTOPP_INCLUDE_PATH}"
+            "${CMAKE_CURRENT_LIST_DIR}/include"
     )
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set_target_properties(
         Cryptopp
         PROPERTIES
         IMPORTED_LOCATION
-            "${CRYPTOPP_STATIC_LIBRARY_PATH}/libcryptopp.a"
+            "${CMAKE_CURRENT_LIST_DIR}/lib/libcryptopp.a"
         IMPORTED_LOCATION_DEBUG
-            "${CRYPTOPP_STATIC_LIBRARY_PATH}/libcryptoppd.a"
+            "${CMAKE_CURRENT_LIST_DIR}/lib/libcryptoppd.a"
         INTERFACE_INCLUDE_DIRECTORIES
-            "${CRYPTOPP_INCLUDE_PATH}"
+            "${CMAKE_CURRENT_LIST_DIR}/include"
     )
 endif()
